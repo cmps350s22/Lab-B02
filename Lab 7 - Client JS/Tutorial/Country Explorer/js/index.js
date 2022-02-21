cfcce9d86ec98958dd167abfffb6034a11b7052b
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-21 15:25:56 +0300

    updated 3:25

--- Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
@@ -5,6 +5,20 @@ const factsArea = document.querySelector('#facts-area')
 const regionBaseUrl = 'https://restcountries.com/v3.1/region'
 const countryBaseUrl = 'https://restcountries.com/v3.1/name'
 
-function getCountries() {
-    alert(`it is connected ${regionSearchBar.value}`)
+async function getCountries() {
+    const url = `${regionBaseUrl}/${regionSearchBar.value}`
+    const data = await fetch(url)
+    const countries = await data.json()
+    const options = countries.map(country => convertCountryToOptions(country))
+    countryDropDown.innerHTML = options.join(' ')
 }
+
+function convertCountryToOptions(country) {
+    return `<option value="${country.name.common}">${country.name.common}</option>`
+}
+
+
+function handleCountryChange(country){
+    alert(`${country}`)
+}
+
