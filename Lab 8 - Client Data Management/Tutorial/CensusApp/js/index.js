daeec0c84814e385a7576503897cbe714c1ad544
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-28 16:40:55 +0300

    Update index.js

--- Lab 8 - Client Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8 - Client Data Management/Tutorial/CensusApp/js/index.js
@@ -5,20 +5,31 @@ const repo = new CensusRepo()
 const countriesTable = document.querySelector('#countries')
 const noOfRowsDD = document.querySelector('#noOfRows')
 const form = document.querySelector('#form')
+const addBtn = document.querySelector('#add-btn')
+
+
 form.addEventListener('submit', addCensus)
 noOfRowsDD.addEventListener('change', showCensusList)
 
 window.onload = async ()=>{
     await showCensusList();
     window.deleteCensus = deleteCensus
+    window.updateCensus = updateCensus
 }
 
 async function addCensus(e) {
     e.preventDefault()
     const census = formToObject(e.target)
-    census.id = Date.now().toString()
-    const response = await repo.addCensus(census)
+
+    if(addBtn.value == 'Add'){
+        census.id = Date.now().toString()
+        const response = await repo.addCensus(census)
+    }else{
+        const response = await repo.updateCensus(census)
+        addBtn.value = 'Add'
+    }
     await showCensusList()
+    form.reset()
 }
 
 async function showCensusList() {
@@ -43,13 +54,19 @@ function censusToHTMLRow(census){
             <td>${census.country}</td>
             <td>${census.population}</td>
             <td>
-                <i class="fa fa-edit">Edit</i>
+                <i class="fa fa-edit" onclick="updateCensus('${census.id}')">Edit</i>
                 <i class="fa fa-trash" onclick="deleteCensus('${census.id}')">Delete</i>
             </td>
         </tr>
     `
 }
-
+async function updateCensus(id){
+    const census = await repo.getCensusById(id)
+    document.querySelector('#country').value = census.country
+    document.querySelector('#population').value = census.population
+    document.querySelector('#id').value = census.id
+    addBtn.value = 'Update'
+}
 async function deleteCensus(id) {
     await repo.deleteCensus(id)
     await showCensusList()
