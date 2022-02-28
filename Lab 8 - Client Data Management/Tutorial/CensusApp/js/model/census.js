6fba64e8f2bf9ce333500e4c142ed87ce26ebddb
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-28 14:38:07 +0300

    updated

---
+++ Lab 8 - Client Data Management/Tutorial/CensusApp/js/model/census.js
@@ -0,0 +1,7 @@
+class Census {
+    constructor(id, country, population) {
+        this.id = id;
+        this.country = country;
+        this.population = population;
+    }
+}
---
+++ Lab 8 - Client Data Management/Tutorial/CensusApp/js/repository/census-repo.js
@@ -0,0 +1,5 @@
+const db = new Localbase('db')
+class CensusRepo {
+
+
+}
