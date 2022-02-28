e20f938e84505eab9febc53b998a7fbfdaf9d9a2
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-28 14:56:59 +0300

    Update census-repo.js

--- Lab 8 - Client Data Management/Tutorial/CensusApp/js/repository/census-repo.js
+++ Lab 8 - Client Data Management/Tutorial/CensusApp/js/repository/census-repo.js
@@ -1,5 +1,47 @@
 const db = new Localbase('db')
+
 class CensusRepo {
+    addCensus(census) {
+        try {
+            return db.collection('census').add(census)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    updateCensus(updateCensus) {
+        try {
+            return db.collection('census').doc({id: updateCensus.id}).update(updateCensus)
+        } catch (e) {
+
+        }
+    }
+
+    deleteCensus(id) {
+        try {
+            return db.collection('census').doc({id}).delete()
+        } catch (e) {
+
+        }
+    }
+
+    getCensusById(id) {
+        try {
+            return db.collection('census').doc({id}).get()
+        } catch (e) {
+
+        }
+    }
+
+    getAllCensus(noOfRows) {
+        try {
+            if (!noOfRows)
+                return db.collection('census').get()
+            return db.collection('census').limit(noOfRows).get()
+        } catch (e) {
+
+        }
+    }
 
 
 }
