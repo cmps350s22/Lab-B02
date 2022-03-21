9757a018d7397b4569bb58c775c651ee8adefc8b
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-21 16:20:21 +0300

    Update app.js

--- Lab 9- Web API/Tutorial/Banking App/app.js
+++ Lab 9- Web API/Tutorial/Banking App/app.js
@@ -1,54 +1,12 @@
 import express from 'express'
+import BankService from "./service/bank-service.js";
+const bankService = new BankService()
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-/*
-    http://www.qu.edu.qa/students
-    http://www.qu.edu.qa/courses
- */
-
-//http://localhost:3000/api/welcome
-//http://localhost:3000/api/accounts
-//http://localhost:3000/api/accounts/1001
-//http://localhost:3000/api/accounts/1001/Abdulahi/100
-
-app.get('/api/welcome', (req, res) => {
-    res.send('welcome to our banking website')
-})
-
-
-app.get('/api/accounts/:acctNo/:name/:age', (req, res)=>{
-    const accounts = [
-        {
-            name : 'Abdulahi',
-            accountNo : 123
-        },
-        {
-            name : 'Saeed',
-            accountNo : 125
-        }
-    ]
-    res.json(req.query)
-})
-
-
-// npm i -g nodemon
-// sudo npm i -g nodemon
-
-
-app.get('/api/accounts/:accountNo', (req, res)=>{
-    res.json(req.params.accountNo)
-})
-
-
-
-
-// app.post()
-// app.put()
-// app.delete()
-
+app.get('/api/accounts', bankService.getAccounts)
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`)
