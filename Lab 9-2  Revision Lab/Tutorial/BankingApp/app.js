60e694cfbb87d7bc13740d69114202730a92dd9b
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-28 14:39:22 +0300

    Update app.js

--- Lab 9-2  Revision Lab/Tutorial/BankingApp/app.js
+++ Lab 9-2  Revision Lab/Tutorial/BankingApp/app.js
@@ -4,6 +4,24 @@
 npm i morgan
 npm i fs-extra
 npm i uuid
+
+nodemon app.js
+nodemon
  */
 
 import express from 'express';
+import morgan from 'morgan'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+/*
+    windows - SET PORT = 5000
+    MAC -EXPORT PORT = 5000
+ */
+
+// middlewares
+app.use(express.static('public'))
+app.listen(port, ()=>{
+    console.log(`server listening on http://localhost:${port}`)
+})
