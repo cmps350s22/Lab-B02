3539c370665b99b4f8a33f42175332f2c376b794
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-28 16:14:27 +0300

    Update account-service.js

--- Lab 9-2  Revision Lab/Tutorial/BankingApp/service/account-service.js
+++ Lab 9-2  Revision Lab/Tutorial/BankingApp/service/account-service.js
@@ -5,7 +5,7 @@ export default class AccountService {
 
     async getAccounts(req, res) {
         try {
-            const accounts = await accountRepo.getAccount(req.query.type)
+            const accounts = await accountRepo.getAccounts(req.query.type)
             res.json(accounts)
         } catch (e) {
             res.status(500).send(e)
