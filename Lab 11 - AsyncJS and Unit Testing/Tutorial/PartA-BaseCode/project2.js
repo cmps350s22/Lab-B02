22adcb9a0271d68522ba60ce7da935471bd6f653
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-11 15:41:43 +0300

    updated

--- Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project2.js
+++ Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project2.js
@@ -9,4 +9,60 @@ function getCourses(cb)
 4)	Instructor names are set as a new property to the course object in the setInstrctorNames function.
 
  */
+import fs from 'fs'
+
+function setStudentsNames(courses, cb) {
+    fs.readFile('data/student.json', (err, data) => {
+        if (!err) {
+            const students = JSON.parse(data)
+
+            for (const course of courses) {
+                course.students = students
+                    .filter(student => student.courseIds.includes(course.crn))
+                    .map(s => s.firstname)
+            }
+            cb(null, courses)
+        } else {
+            cb(err, null)
+        }
+    })
+}
+
+function setInstructorNames(courses, cb) {
+    fs.readFile('data/staff.json', (err, data) => {
+        if (!err) {
+            const staffs = JSON.parse(data)
+            for (const course of courses) {
+                const {firstname, lastname} = staffs.find(staff => staff.staffNo == course.instructorId)
+                course.instructor = `${firstname} ${lastname}`
+            }
+            cb(null, courses)
+        } else {
+            cb(err, null)
+        }
+    })
+}
+
+function getCourses(cb) {
+    fs.readFile('data/course.json', (err, data) => {
+        if (!err) {
+            const courses = JSON.parse(data)
+            setInstructorNames(courses, (err, data) => {
+                if (!err)
+                    setStudentsNames(courses, cb)
+                else {
+                    cb(err, null)
+                }
+            })
+        } else {
+            cb(err, null)
+        }
+
+    })
+}
+
+getCourses((err, data) => {
+    if (!err) console.log(data)
+})
+
 
--- Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project4.js
+++ Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project4.js
@@ -1,7 +1,11 @@
-
-const fs=require('fs-extra');
-
-
 /*
 1.Rewrite the you created in Part-2 using promises.
 */
+
+import fs from 'fs-extra';
+
+fs.readJson('data/course.json')
+    .then(courses => {
+        console.log(courses)
+    })
+    .catch(err => console.error(err))
